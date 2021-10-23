a3d4e3ebcfc4efa0821a1915da860a39aebb3b97
Muthun0916 <76974949+Muthun0916@users.noreply.github.com>
2021-10-24 01:49:26 +0900

    Revert "Revert "Update setting.py""
    This reverts commit 497dc4edcd3193a2d742f227cd5d86aa7cc6c604.

--- setting.py
+++ setting.py
@@ -9,14 +9,33 @@ from os.path import expanduser
 
 
 def main():
-    forgename="forge-1.12.2-14.23.5.2855-installer.jar"
     modsname="mods.zip"
     home=expanduser("~")
     path=home+"\\AppData\\Roaming\\.minecraft\\mods"
+    isForge = False
+    version = None
+    forgename="forge-"+str(version)+"-installer.jar"
 
-    if not os.path.exists(forgename):
+    while True:
+        isForgeQ = input("Forgeをダウンロードしますか？(y/n)").lower()
+        if isForgeQ[0]=="y":
+            isForge = True
+            break
+        elif isForgeQ[0]=="n":
+            break
+
+
+
+    if isForge == True:
+
+        while True:
+            print("ダウンロードしたいForgeのバージョンを入力してください。")
+            version = input("入力例:1.12.2-14.23.5.2855")
+            if version is not None:
+                break
+
         print(forgename+"をダウンロードします")
-        url="https://maven.minecraftforge.net/net/minecraftforge/forge/1.12.2-14.23.5.2855/forge-1.12.2-14.23.5.2855-installer.jar"
+        url="https://maven.minecraftforge.net/net/minecraftforge/forge/"+version+"/"+forgename
         for i in tqdm.tqdm(range(int(1e7))):
             np.pi*np.pi
         urlData = requests.get(url).content
@@ -25,22 +44,42 @@ def main():
         with open(forgename,mode="wb") as f:
             f.write(urlData)
 
-    print(forgename+"を確認")
+        print(forgename+"を確認")
 
     if not os.path.exists(modsname):
-        print("https://drive.google.com/drive/folders/1t59L1wWUyg7aihSYvY1ok1IuYMCUUJ-j"+\
-        "からダウンロードしたzipを "+modsname+" にリネームして同じ階層に配置してください。")
-        input("続けるには何かキーをおしてください")
+        print("Error : ModsFile is not in same directry.")
+        print("modが入ったzipを "+modsname+" にリネームして同じ階層に配置してください。")
+        input("Enterを押すと終了します...")
         return
 
     if not os.path.exists(path):
         os.mkdir(path)
         print("modsファイルを新規作成しました")
     else:
-        print("modsファイルのバックアップを作成します")
-        dt = str(datetime.today()).replace(".","").replace(":","").replace("-","").replace(" ","")
-        shutil.make_archive(home+"\\AppData\\Roaming\\.minecraft\\"+str(dt)+"mods","zip",path)
-        print("modsファイルのバックアップを作成しました")
+        isApdQ = False
+
+        while True:
+
+            print("すでにmodsファイルが存在しています！バックアップしますか？")
+            isApd = input("※zipファイルで別途保存します(y/n)").lower()
+
+            if isApd[0]=="y":
+                isApdQ = True
+                break
+            elif isApd[0]=="n":
+                break
+
+        if isApdQ == True:
+            print("modsファイルのバックアップを作成します")
+            for i in tqdm.tqdm(range(int(1e7))):
+                np.pi*np.pi
+            """
+            urlData = requests.get(url).content
+            """
+            dt = str(datetime.today()).replace(".","").replace(":","").replace("-","").replace(" ","")
+            shutil.make_archive(home+"\\AppData\\Roaming\\.minecraft\\"+str(dt)+"mods","zip",path)
+            print("modsファイルのバックアップを作成しました")
+
         shutil.rmtree(path)
         os.mkdir(path)
 
@@ -48,7 +87,7 @@ def main():
         existnig_zip.extractall(path)
         print("modsの導入に成功しました")
 
-    input("続けるには何かキーをおしてください")
+    input("Enterを押すと終了します...")
 
 
 
